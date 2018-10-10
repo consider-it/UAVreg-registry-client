73f5b6bac704fd4b1ac773cabb5b5c736da27f58
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2018-10-10 16:02:10 +0200

    [CODE] send data to registry API

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const buildVersion string = "v0.0.1"
@@ -17,18 +22,18 @@ const droneIDByteLen = 18
 const droneIDHexLen = 2 * droneIDByteLen
 
 var droneID string
-var revoke bool
+var apiURL string
 
 func init() {
 	flag.StringVar(&droneID, "d", "0", "drone ID as hex string")
-	flag.BoolVar(&revoke, "r", false, "revoke insurance for this drone")
+	flag.StringVar(&apiURL, "u", "http://localhost:8080/registry", "URL to send the POST request to")
 	flag.Parse()
 }
 
 func main() {
 	fmt.Println("droneregistry client " + buildVersion)
 	fmt.Println("- drone ID: ", droneID)
-	fmt.Println("- revoke?:  ", revoke)
+	fmt.Println("- url:      ", apiURL)
 	fmt.Println("")
 
 	// check inputs
@@ -49,6 +54,32 @@ func main() {
 		log.Fatalln("fatal error:\n", err)
 	}
 
-	// TODO: actually send data to dronePKI
-	log.Fatalln("TODO: nothing implemented yet")
+	// auto-set valdity from now until now + 2 months
+	validFrom := time.Now()
+	validUntil := time.Now().AddDate(0, 2, 0)
+
+	var jsonStr = []byte(`[{"droneId": "` + droneID + `", "validFrom": "` + validFrom.Format(time.RFC3339) + `", "validUntil": "` + validUntil.Format(time.RFC3339) + `"}]`)
+
+	// send data to dronePKI
+	fmt.Println("sending request to registry...")
+	req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("fatal error:\n", err)
+	}
+	defer resp.Body.Close()
+
+	// check for response status 200
+	if strings.HasPrefix(resp.Status, "200") {
+		fmt.Print("SUCCESS: ")
+	} else {
+		fmt.Println("ERROR:", resp.Status)
+		fmt.Print(" >>>>  ")
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
